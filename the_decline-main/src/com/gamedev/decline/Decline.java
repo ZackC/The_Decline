f4a08d3d4687dcbac5f01b21e9a7e79da6f999b0
Jake <jake@cobaltbrothers.com>
2013-03-06 09:24:56 -0600

    Added left/right movement

--- the_decline-main/src/com/gamedev/decline/Decline.java
+++ the_decline-main/src/com/gamedev/decline/Decline.java
@@ -2,6 +2,8 @@ package com.gamedev.decline;
 
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input.Keys;
+import com.badlogic.gdx.graphics.FPSLogger;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
@@ -12,25 +14,28 @@ public class Decline implements ApplicationListener {
 	private OrthographicCamera camera;
 	private SpriteBatch batch;
 	private Sprite character;
-	
+	private int heroSpeed = 200;
+	private FPSLogger fps = new FPSLogger();
+
 	@Override
 	public void create() {		
 		float width = Gdx.graphics.getWidth();
 		float height = Gdx.graphics.getHeight();
-		
+
 		Gdx.app.log("Screen Width", String.valueOf(width));
 		Gdx.app.log("Screen Height", String.valueOf(height));
-		
+
 		camera = new OrthographicCamera(width, height);
+		camera.setToOrtho(false);
 		batch = new SpriteBatch();
-		
+
 		character = new Sprite(new Texture(Gdx.files.internal("hero_weapon.png")));
 		character.setOrigin(character.getWidth()/2, character.getHeight()/2);
-		character.setPosition(-character.getWidth()/2, -character.getHeight()/2);
-		
+		character.setPosition(20, 20);
+
 		Gdx.app.log("Character Width", String.valueOf(character.getWidth()));
 		Gdx.app.log("Character Height", String.valueOf(character.getHeight()));
-		
+
 	}
 
 	@Override
@@ -42,17 +47,31 @@ public class Decline implements ApplicationListener {
 	public void render() {		
 		Gdx.gl.glClearColor(1, 1, 1, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		
+
 		batch.setProjectionMatrix(camera.combined);
 		batch.begin();
 		character.draw(batch);
 		batch.end();
-		
+
 		update();
+		//fps.log();
+		
 	}
-	
+
 	private void update() {
 		
+		if(character.getX() > 0)
+			if(Gdx.input.isKeyPressed(Keys.LEFT)) {
+				character.setX(character.getX() - heroSpeed * Gdx.graphics.getDeltaTime());
+				if(character.isFlipX() == false)
+					character.flip(true, false);
+			}
+		if(character.getX()+character.getWidth() < Gdx.graphics.getWidth())
+			if(Gdx.input.isKeyPressed(Keys.RIGHT)) {
+				character.setX(character.getX() + heroSpeed * Gdx.graphics.getDeltaTime());
+				if(character.isFlipX() == true)
+					character.flip(true, false);
+			}
 	}
 
 	@Override
