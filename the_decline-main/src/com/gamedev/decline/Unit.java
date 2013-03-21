c82b4bb0798f4dd52f7f65d3ee287549ac390c97
Zach Ogle <zach_ogle@bellsouth.net>
2013-03-21 14:41:03 -0500

    Hero and Unit Edits

--- the_decline-main/src/com/gamedev/decline/Unit.java
+++ the_decline-main/src/com/gamedev/decline/Unit.java
@@ -11,16 +11,17 @@ import com.badlogic.gdx.graphics.g2d.TextureRegion;
 public abstract class Unit extends Sprite{
 	
 	protected GlobalSingleton gs = GlobalSingleton.getInstance();
-	protected double xPos;
-	protected double yPos;
+	protected float xPos;
+	protected float yPos;
 	protected int speed;
 	protected int height;
 	protected int width;
 	
-	public Unit(Texture texture, int speed, double xPos, double yPos){
+	public Unit(Texture texture, int speed, float xPos, float yPos){
 		super(texture);
 		height = texture.getHeight();
 		width = texture.getWidth();
+		this.speed = speed;
 	}
 	
 	public void setSpeed(int newSpeed){
@@ -31,21 +32,21 @@ public abstract class Unit extends Sprite{
 		return speed;
 	}
 	
-	public void setXPos(double newXPos)
+	public void setXPos(float newXPos)
 	{
 		xPos = newXPos;
 	}
 	
-	public double getXPos()
+	public float getXPos()
 	{
 		return xPos;
 	}
 	
-	public void setYPos(double newYPos){
+	public void setYPos(float newYPos){
 		yPos = newYPos;
 	}
 	
-	public double getYPos(){
+	public float getYPos(){
 		return yPos;
 	}
 	
